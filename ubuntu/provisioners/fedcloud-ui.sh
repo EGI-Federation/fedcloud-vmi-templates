ce7fe1e63297d261e0dfd6bbf27dde9f816df398
Enol Fernandez <enolfc@ifca.unican.es>
2016-04-28 14:21:31 +0200

    FedCloud UI improvements

--- ubuntu/provisioners/fedcloud-ui.sh
+++ ubuntu/provisioners/fedcloud-ui.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-curl -L http://go.egi.eu/fedcloud.ui | /bin/bash -
+curl -L http://go.egi.eu/fedcloud.ui > /tmp/fedcloud.ui
+cat /tmp/fedcloud.ui | bash -
+# this is here until I understand what's going on with the script
+cat /tmp/fedcloud.ui | bash -
+
+rm -rf /tmp/fedcloud.ui
