182cd1c3c8e50e4574eaf5e70c7ce375c466c310
Enol Fernandez <enolfc@ifca.unican.es>
2016-07-29 11:26:12 +0100

    New provisioner for fedcloud development environment

---
+++ ubuntu/provisioners/fedcloud-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get -y update
+
+# some basic tools
+apt-get install -y vim git
+
+# Java env
+apt-get install -y openjdk-7-jre openjdk-7-jre-lib openjdk-7-jdk maven
+apt-get install ca-policy-egi-core
+
+# Python
+apt-get -y install python-pip python-dev libffi-dev libssl-dev
+pip install python-keystoneclient python-swiftclient \
+            python-openstackclient PyOpenSSL xmltodict \
+            openstack-voms-auth-type
+# Add certificates to python requests
+cat /etc/grid-security/certificates/*.pem >> $(python -m requests.certs)
