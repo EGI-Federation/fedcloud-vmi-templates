e51405c1519cf0833a7b67512a20cb93bc2926b0
Enol Fernandez <enolfc@ifca.unican.es>
2016-08-03 08:46:11 +0100

    Version 2016.08.03, added JAVA_HOME to environment

--- ubuntu/provisioners/fedcloud-dev.sh
+++ ubuntu/provisioners/fedcloud-dev.sh
@@ -8,6 +8,9 @@ apt-get install -y vim git
 # Java env
 apt-get install -y openjdk-7-jre openjdk-7-jre-lib openjdk-7-jdk maven
 apt-get install ca-policy-egi-core
+echo 'JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"' >> /etc/environment
+
+
 
 # Python
 apt-get -y install python-pip python-dev libffi-dev libssl-dev
