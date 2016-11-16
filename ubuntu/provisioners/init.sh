f8a62374d1c549da4c5de291296b89ba3060b797
Enol Fernandez <enol.fernandez@egi.eu>
2016-11-16 12:00:34 +0000

    Assure rebooting in ubuntu 16.04

--- ubuntu/provisioners/init.sh
+++ ubuntu/provisioners/init.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+set -x
+
 # update already installed packages
 apt-get -q update
 apt-get -q --assume-yes upgrade
 
+
+# shutdown ssh so reboot works (ubuntu 16.04)
+service ssh stop
+sleep 10s 
 # reboot so we have the working kernel updated
-reboot
+reboot -f
+
 sleep 90s
