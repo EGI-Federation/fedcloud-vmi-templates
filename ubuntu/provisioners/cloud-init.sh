ab7fa5ede0b671d97806af80d73f35bb407ffbbf
Enol Fernandez <enol.fernandez@egi.eu>
2017-01-10 13:56:11 +0000

    Apply patch to Ubuntu 14.04 and use new config

--- ubuntu/provisioners/cloud-init.sh
+++ ubuntu/provisioners/cloud-init.sh
@@ -12,4 +12,17 @@ if [ "x$(lsb_release -rs)" == "x12.04" ]; then
   apt-get -q --assume-yes install cloud-utils-growpart
 fi
 
-mv /root/cloud.cfg /etc/cloud/cloud.cfg
+if [ "x$(lsb_release -rs)" == "x14.04" ]; then
+  pushd /usr/lib/python2.7/dist-packages/cloudinit/distros/
+  patch -p 1 <  /root/cloud-init.patch
+  popd
+  # Create .pyc files
+  rm /usr/lib/python2.7/dist-packages/cloudinit/distros/__init__.pyc \
+     /usr/lib/python2.7/dist-packages/cloudinit/distros/debian.pyc \
+     /usr/lib/python2.7/dist-packages/cloudinit/distros/rhel.pyc \
+     /usr/lib/python2.7/dist-packages/cloudinit/distros/sles.pyc
+  python -c "import cloudinit.distros.debian; import cloudinit.distros.rhel; import cloudinit.distros.sles"
+  rm /root/cloud-init.patch
+fi
+
+mv /root/fedcloud.cfg /etc/cloud/cloud.cfg.d/01_fedcloud.cfg
