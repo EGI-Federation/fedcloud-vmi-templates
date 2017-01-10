3f41ba782812c019d8a3e1b91b4b98c3ed659e26
Enol Fernandez <enol.fernandez@egi.eu>
2017-01-10 13:56:57 +0000

    Make this work both in 14.04 and 16.04

--- ubuntu/provisioners/init.sh
+++ ubuntu/provisioners/init.sh
@@ -7,10 +7,9 @@ apt-get -q update
 apt-get -q --assume-yes upgrade
 
 
-# shutdown ssh so reboot works (ubuntu 16.04)
-service ssh stop
-sleep 10s 
 # reboot so we have the working kernel updated
-reboot -f
+# shutdown ssh so reboot works (ubuntu 16.04)
+release=$(lsb_release -r -s)
+[[ $release =~ ^16 ]] && (service ssh stop && sleep 10s && reboot -f) || reboot
 
 sleep 90s
