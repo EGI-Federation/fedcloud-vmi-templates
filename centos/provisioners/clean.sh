9870f7f96339e230e9010982ef2ebd041d39b14c
Enol Fernandez <enol.fernandez@egi.eu>
2017-06-21 08:45:33 +0200

    Remove network configuration from clean

--- centos/provisioners/clean.sh
+++ centos/provisioners/clean.sh
@@ -9,20 +9,6 @@ sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 # 3. disable password authentication (cloud-init should also do this, but just in case)
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
 
-
-# remove hardware address (MAC) and UUID from NIC configuration files
-sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth*
-sed -i '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth*
-
-# make sure nothing is messing with NICs' MAC adresses
-unlink /etc/udev/rules.d/70-persistent-net.rules
-ln -s /dev/null /etc/udev/rules.d/70-persistent-net.rules
-unlink /etc/udev/rules.d/70-persistent-cd.rules
-ln -s /dev/null /etc/udev/rules.d/70-persistent-cd.rules
-
-chkconfig network on
-echo "NOZEROCONF=yes" >> /etc/sysconfig/network
-
 # look root password
 passwd -l root
 
