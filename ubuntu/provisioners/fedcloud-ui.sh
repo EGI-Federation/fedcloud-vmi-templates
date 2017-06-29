2407dc26e88105ab88bcf8d31e383b49c7416017
Enol Fernandez <enol.fernandez@egi.eu>
2017-06-29 13:20:04 +0100

    Improve UI installation

--- ubuntu/provisioners/fedcloud-ui.sh
+++ ubuntu/provisioners/fedcloud-ui.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+set -exuo pipefail
+
 curl -L https://raw.githubusercontent.com/EGI-FCTF/fedcloud-userinterface/master/fedcloud-ui.sh > /tmp/fedcloud.ui
+
+sed -i 's/# Some variables.*/set -exuo pipefail/' /tmp/fedcloud.ui
+
 cat /tmp/fedcloud.ui | bash -
 # this is here until I understand what's going on with the script
-cat /tmp/fedcloud.ui | bash -
+# cat /tmp/fedcloud.ui | bash -
 
 rm -rf /tmp/fedcloud.ui
 
 # prevent clock skew
-sudo apt-get install -y ntp
-service start ntp
+apt-get install -y ntp
+service ntp start
 
 # Install VBoxGuestAdditions
-apt-get update
-apt-get install dkms
+apt-get install -y dkms
 
 vbox_latest=$(curl http://download.virtualbox.org/virtualbox/LATEST.TXT)
 curl http://download.virtualbox.org/virtualbox/$vbox_latest/VBoxGuestAdditions_$vbox_latest.iso > /tmp/vbox.iso
 mount -o loop,ro /tmp/vbox.iso /mnt
 pushd /mnt
-./VBoxLinuxAdditions.run
+# don't fail here
+./VBoxLinuxAdditions.run || true
 popd
 umount /mnt
 rm -rf /tmp/vbox.iso
