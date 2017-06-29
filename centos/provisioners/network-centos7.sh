af44ef521af19d46605cab1c1c4bebe5089b27e1
Enol Fernandez <enol.fernandez@egi.eu>
2017-06-29 13:19:32 +0100

    Updates for centos

--- centos/provisioners/network-centos7.sh
+++ centos/provisioners/network-centos7.sh
@@ -19,21 +19,21 @@ IPV6INIT="no"
 PERSISTENT_DHCLIENT="1"
 EOF
 
-rm /etc/udev/rules.d/80-net-name-slot.rules
-ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules 
+#rm /etc/udev/rules.d/80-net-name-slot.rules
+#ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules 
 
-rm /etc/udev/rules.d/80-net-setup-link.rules
-ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+#rm /etc/udev/rules.d/80-net-setup-link.rules
+#ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 
 # remove hardware address (MAC) and UUID from NIC configuration files
-sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth*
-sed -i '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth*
+#sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth*
+#sed -i '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth*
 
 # make sure nothing is messing with NICs' MAC adresses
-unlink /etc/udev/rules.d/70-persistent-net.rules
-ln -s /dev/null /etc/udev/rules.d/70-persistent-net.rules
-unlink /etc/udev/rules.d/70-persistent-cd.rules
-ln -s /dev/null /etc/udev/rules.d/70-persistent-cd.rules
+#unlink /etc/udev/rules.d/70-persistent-net.rules
+#ln -s /dev/null /etc/udev/rules.d/70-persistent-net.rules
+#unlink /etc/udev/rules.d/70-persistent-cd.rules
+#ln -s /dev/null /etc/udev/rules.d/70-persistent-cd.rules
 
 # enable built-in networking
 # using both commands because of unfinished systemd support in system
