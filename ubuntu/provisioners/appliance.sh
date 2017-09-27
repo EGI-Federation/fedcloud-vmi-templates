0e9762b051ff1561a09fe85c299284007ffcf1a6
Enol Fernandez <enol.fernandez@egi.eu>
2017-09-27 10:17:42 +0200

    Appliance update for new containers

--- ubuntu/provisioners/appliance.sh
+++ ubuntu/provisioners/appliance.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+set -uexo pipefail
+
+APPLIANCE_DIR=/tmp/fedcloudappliance
 
 # Get git repo and popoulate default config
-git clone https://github.com/enolfc/fedcloudappliance.git /tmp/fedcloudappliance
+git clone https://github.com/enolfc/fedcloudappliance.git $APPLIANCE_DIR
 
-mkdir -p /etc/atrope \
+mkdir -p /etc/cloudkeeper \
          /etc/apel \
          /etc/caso \
          /etc/cloud-info-provider \
@@ -16,21 +16,30 @@ mkdir -p /etc/atrope \
          /var/spool/apel \
          /image_data
 
-cp /tmp/fedcloudappliance/conf/atrope/ /etc/atrope/
-cp /tmp/fedcloudappliance/conf/atrope/* /etc/atrope/
-cp /tmp/fedcloudappliance/conf/apel/* /etc/apel/
-cp /tmp/fedcloudappliance/conf/caso/* /etc/caso
-cp /tmp/fedcloudappliance/conf/voms.json /etc/
-cp /tmp/fedcloudappliance/conf/cron.d/* /etc/cron.d
-cp /tmp/fedcloudappliance/conf/bdii/openstack.rc /etc/cloud-info-provider/
-cp /tmp/fedcloudappliance/conf/bdii/openstack.yaml /etc/cloud-info-provider/
-cp /tmp/fedcloudappliance/conf/bdii/site.cfg /etc/sitebdii/
-cp /tmp/fedcloudappliance/conf/bdii/glite-info-site-defaults.conf /etc/sitebdii/
-cp /tmp/fedcloudappliance/conf/bdii/docker-compose.yml /etc/sitebdii/
-cp /tmp/fedcloudappliance/conf/scripts/* /usr/local/bin/
-chmod +x /usr/local/bin/*
+pushd $APPLIANCE_DIR
 
-rm -rf /tmp/fedcloudappliance
+cp conf/voms.json /etc/
+cp conf/cloudkeeper/cloudkeeper-os.conf conf/cloudkeeper/cloudkeeper.yml \
+   conf/cloudkeeper/image-lists.conf \
+   /etc/cloudkeeper/
+cp conf/cloudkeeper/cloudkeeper-os.service /etc/systemd/system/
+cp conf/caso/caso.conf /etc/caso
+cp conf/cron.d/* /etc/cron.d
+cp conf/bdii/openstack.rc /etc/cloud-info-provider/
+cp conf/bdii/openstack.yaml /etc/cloud-info-provider/
+cp conf/bdii/site.cfg /etc/sitebdii/
+cp conf/bdii/glite-info-site-defaults.conf /etc/sitebdii/
+cp conf/bdii/docker-compose.yml /etc/sitebdii/
+cp conf/bdii/bdii.service /etc/systemd/system/ 
+cp conf/scripts/* /usr/local/bin/
+
+popd
+
+chmod +x /usr/local/bin/*
+systemctl enable bdii
+systemctl enable cloudkeeper-os 
+
+rm -rf $APPLIANCE_DIR
 
 # Install CAs and fetch-crl
 curl https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 | apt-key add - 
