c29076ad1fc9358c8a0393ec5742c918bcdffc7f
Enol Fernandez <enol.fernandez@egi.eu>
2018-05-04 15:56:27 +0300

    Adapt to the latest updates in appliance
    Use tagged containers and create VM specific config here.

--- ubuntu/provisioners/appliance.sh
+++ ubuntu/provisioners/appliance.sh
@@ -3,11 +3,13 @@
 set -uexo pipefail
 
 APPLIANCE_DIR=/tmp/fedcloudappliance
+TAG=v0.1
 
 # Get git repo and popoulate default config
-git clone https://github.com/enolfc/fedcloudappliance.git $APPLIANCE_DIR
+git clone --branch $TAG https://github.com/enolfc/fedcloudappliance.git $APPLIANCE_DIR
 
 mkdir -p /etc/cloudkeeper \
+         /etc/cloudkeeper-os \
          /etc/apel \
          /etc/caso \
          /etc/cloud-info-provider \
@@ -18,33 +20,134 @@ mkdir -p /etc/cloudkeeper \
 
 pushd $APPLIANCE_DIR
 
-cp conf/voms.json /etc/
-cp conf/cloudkeeper/cloudkeeper-os.conf conf/cloudkeeper/cloudkeeper.yml \
-   conf/cloudkeeper/image-lists.conf \
-   /etc/cloudkeeper/
-cp conf/cloudkeeper/cloudkeeper-os.service /etc/systemd/system/
-cp conf/caso/caso.conf /etc/caso
-cp conf/cron.d/* /etc/cron.d
-cp conf/bdii/openstack.rc /etc/cloud-info-provider/
-cp conf/bdii/openstack.yaml /etc/cloud-info-provider/
-cp conf/bdii/site.cfg /etc/sitebdii/
-cp conf/bdii/glite-info-site-defaults.conf /etc/sitebdii/
-cp conf/bdii/docker-compose.yml /etc/sitebdii/
-cp conf/bdii/bdii.service /etc/systemd/system/ 
-cp conf/scripts/* /usr/local/bin/
+# cloudkeeper-os
+cp cloudkeeper/os/cloudkeeper-os.conf /etc/cloudkeeper-os/
+cp cloudkeeper/os/voms.json  /etc/cloudkeeper-os/
+
+cat > /etc/systemd/system/cloudkeeper-os.service << EOF
+#
+# This manages the cloudkeeper OS backend
+#
+[Unit]
+Description=CloudKeeper Service
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStartPre=-/usr/bin/docker stop cloudkeeper-os
+ExecStartPre=-/usr/bin/docker rm -v cloudkeeper-os
+ExecStart=/usr/bin/docker run --name cloudkeeper-os -v /etc/cloudkeeper-os/cloudkeeper-os.conf:/etc/cloudkeeper-os/cloudkeeper-os.conf -v /etc/cloudkeeper-os/voms.json:/etc/cloudkeeper-os/voms.json -v /image_data:/var/spool/cloudkeeper/images egifedcloud/cloudkeeper-os:$TAG
+ExecStop=/usr/bin/docker stop cloudkeeper-os
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# cloudkeeper core
+cp cloudkeeper/core/image-lists.conf /etc/cloudkeeper
+cp cloudkeeper/core/cloudkeeper.yml /etc/cloudkeeper
+cp cloudkeeper/core/cloudkeeper.cron /etc/cron.d/
+
+cat > /usr/local/bin/cloudkeeper.sh << EOF
+#!/bin/sh
+
+docker run -v /etc/grid-security:/etc/grid-security \
+           -v /etc/cloudkeeper:/etc/cloudkeeper \
+           -v /image_data:/var/spool/cloudkeeper/images \
+           --link cloudkeeper-os:backend \
+           --rm egifedcloud/cloudkeeper:$TAG cloudkeeper sync --debug
+EOF
+
+# caso
+cp accounting/caso/voms.json /etc/caso
+cp accounting/caso/caso.conf /etc/caso
+cp accounting/caso/caso.cron /etc/cron.d/
+
+cat > /usr/local/bin/caso-extract.sh << EOF
+#!/bin/sh
+
+docker run -v /etc/caso/voms.json:/etc/caso/voms.json \
+           -v /etc/caso/caso.conf:/etc/caso/caso.conf \
+           -v /var/spool/caso:/var/spool/caso \
+           -v /var/spool/apel:/var/spool/apel \
+           --rm egifedcloud/caso:$TAG
+EOF
+
+
+# ssm
+cp accounting/ssm/logging.cfg /etc/apel
+cp accounting/ssm/sender.cfg /etc/apel
+cp accounting/ssm/ssmsend.cron /etc/cron.d
+cat > /usr/local/bin/ssm-send.sh << EOF
+#!/bin/sh
+
+docker run -v /etc/grid-security:/etc/grid-security \
+           -v /var/spool/apel:/var/spool/apel \
+           --rm egifedcloud/ssm:$TAG ssmsend
+EOF
+
+# cloud-info
+cp cloud-info/cloud/openstack.rc /etc/cloud-info-provider/
+cp cloud-info/cloud/openstack.yaml /etc/cloud-info-provider/
+
+# site-bdii
+cat > /etc/sitebdii/docker-compose.yml << EOF
+version: '2'
+services:
+  cloudbdii:
+    image: egifedcloud/cloudbdii:$TAG
+    volumes:
+     - /etc/cloud-info-provider/openstack.rc:/etc/cloud-info-provider/openstack.rc
+     - /etc/cloud-info-provider/openstack.yaml:/etc/cloud-info-provider/openstack.yaml
+  sitebdii:
+    image: egifedcloud/sitebdii:$TAG
+    volumes:
+     - /etc/sitebdii/site.cfg:/etc/glite-info-static/site/site.cfg
+     - /etc/sitebdii/glite-info-site-defaults.conf:/etc/bdii/gip/glite-info-site-defaults.conf
+    links:
+     - cloudbdii
+    ports:
+     - "2170:2170"
+EOF
+
+cat > /etc/systemd/system/bdii.service << EOF
+#
+# This manages the docker-compose for BDII
+#
+[Unit]
+Description=BDII Service
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStartPre=/usr/local/bin/docker-compose -f /etc/sitebdii/docker-compose.yml rm -v -f -s
+ExecStart=/usr/local/bin/docker-compose -f /etc/sitebdii/docker-compose.yml up
+ExecStop=/usr/local/bin/docker-compose -f /etc/sitebdii/docker-compose.yml stop
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cp cloud-info/site/site.cfg /etc/sitebdii/
+cp cloud-info/site/glite-info-site-defaults.conf /etc/sitebdii/
 
 popd
 
 chmod +x /usr/local/bin/*
 systemctl enable bdii
-systemctl enable cloudkeeper-os 
+systemctl enable cloudkeeper-os
 
 rm -rf $APPLIANCE_DIR
 
 # Install CAs and fetch-crl
-curl https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 | apt-key add - 
-echo "deb http://repository.egi.eu/sw/production/cas/1/current egi-igtf core" >> /etc/apt/sources.list.d/egi-cas.list 
+curl https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 | apt-key add -
+echo "deb http://repository.egi.eu/sw/production/cas/1/current egi-igtf core" >> /etc/apt/sources.list.d/egi-cas.list
 apt-get update
 apt-get -qy install --fix-missing ca-policy-egi-core fetch-crl
 
-fetch-crl || true 
+IMAGES="cloudkeeper cloudkeeper-os bdii sitebdii cloudbdii caso ssm"
+for i in $IMAGES; do
+    docker pull egifedcloud/$i:$TAG
+done
+
+fetch-crl -p 2 -T 30 || true
