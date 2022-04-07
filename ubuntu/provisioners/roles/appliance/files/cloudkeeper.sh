d38f471e035a99c5f1de5e35643ac8a1577396a7
Enol Fernandez <enol.fernandez@egi.eu>
2022-04-07 09:43:37 +0100

    Fixes in appliance

--- ubuntu/provisioners/roles/appliance/files/cloudkeeper.sh
+++ ubuntu/provisioners/roles/appliance/files/cloudkeeper.sh
@@ -5,6 +5,7 @@
 docker run -v /etc/grid-security:/etc/grid-security \
            -v /etc/cloudkeeper:/etc/cloudkeeper \
            -v /var/lock/cloudkeeper:/var/lock/cloudkeeper \
+           -v /etc/cloudkeeper/entrypoint.sh:/entrypoint.sh \
            -v /image_data:/var/spool/cloudkeeper/images \
            --link cloudkeeper-os:backend \
            --rm $CLOUDKEEPER_CORE_IMAGE cloudkeeper sync --debug
