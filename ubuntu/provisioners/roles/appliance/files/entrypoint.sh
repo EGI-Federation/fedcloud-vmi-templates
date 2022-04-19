37c68d29e29e4642cd00bb28d5330484997db3bb
Enol Fernandez <enol.fernandez@egi.eu>
2022-04-19 09:57:59 +0100

    Fix entrypoint

--- ubuntu/provisioners/roles/appliance/files/entrypoint.sh
+++ ubuntu/provisioners/roles/appliance/files/entrypoint.sh
@@ -4,10 +4,10 @@ if [ "$DEBUG" = "1" ] ; then
     set -x
 fi
 
-EXTRA_OPTS=""
+EXTRA_OPTS=()
 
 if [ "$BACKEND_PORT_50051_TCP_ADDR" != "" ]; then
-    EXTRA_OPTS="$EXTRA_OPTS --backend-endpoint=$BACKEND_PORT_50051_TCP_ADDR:$BACKEND_PORT_50051_TCP_PORT"
+    EXTRA_OPTS=("${EXTRA_OPTS[@]}" --backend-endpoint="$BACKEND_PORT_50051_TCP_ADDR:$BACKEND_PORT_50051_TCP_PORT")
 fi
 
-exec "$@" "$EXTRA_OPTS"
+exec "$@" "${EXTRA_OPTS[@]}"
