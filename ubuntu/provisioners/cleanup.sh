7b6774fcee2e1e1f9912c74d62d9af2c33b83677
Enol Fernandez <enol.fernandez@egi.eu>
2022-11-03 16:21:21 +0000

    Improve clenaup

--- ubuntu/provisioners/cleanup.sh
+++ ubuntu/provisioners/cleanup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
 
-# See https://github.com/NeCTAR-RC/nectar-images/blob/master/scripts/cleanup.sh
-
 # Clean up leftover build files
-rm -fr /home/*/{.ssh,.ansible,.cache}
-rm -fr /root/{.ssh,.ansible,.cache}
-rm -fr /root/'~'*
+sudo rm -fr /home/*/{.ssh,.ansible,.cache}
+sudo rm -fr /root/{.ssh,.ansible,.cache}
+sudo rm -fr /root/'~'*
