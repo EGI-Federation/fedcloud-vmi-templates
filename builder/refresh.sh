f268ee4568c934b01227be19aa60221e1b40a2d5
Enol Fernandez <enol.fernandez@egi.eu>
2024-08-02 12:15:03 +0100

    Another attempt

--- builder/refresh.sh
+++ builder/refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# one script to avoid repetition in GitHub actions
+# one fundtion to avoid repetition in GitHub actions
 # takes as params:
 # 1 --> the vo to use for entitlement
 # 2 --> the refresh token
@@ -7,29 +7,30 @@
 #
 # Will throw the OIDC TOKEN to output!
 
-set -ex
 
-VO="$1"
-shift
-REFRESH_TOKEN="$2"
-shift
+refresh_token() {
+	local vo="$1"
+	shift
+	local token="$2"
+	shift
 
-# using parametric scopes to only have access to the right VO
-SCOPE="openid%20email%20profile%20voperson_id"
-SCOPE="$SCOPE%20eduperson_entitlement:urn:mace:egi.eu:group:$VO:role=vm_operator#aai.egi.eu"
-SCOPE="$SCOPE%20eduperson_entitlement:urn:mace:egi.eu:group:$VO:role=member#aai.egi.eu"
-OIDC_TOKEN=$(curl -X POST "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/token" \
-                  -d "grant_type=refresh_token&client_id=token-portal&scope=$SCOPE&refresh_token=$REFRESH_TOKEN" \
-                  | jq -r ".access_token")
-echo "::add-mask::$OIDC_TOKEN"
-echo $@
-for cloud in "$@" ; do
-	echo $cloud
-	SITE="$(yq -r ".clouds.$cloud.site" clouds.yaml)"
-	VO="$(yq -r ".clouds.$cloud.vo" clouds.yaml)"
-	OS_TOKEN="$(fedcloud openstack token issue --oidc-access-token "$OIDC_TOKEN" \
-        			--site "$SITE" --vo "$VO" -j | jq -r '.[0].Result.id')"
-	yq -y -i '.clouds.'"$cloud"'.auth.token="'"$OS_TOKEN"'"'  clouds.yaml
-done
+	# using parametric scopes to only have access to the right VO
+	SCOPE="openid%20email%20profile%20voperson_id"
+	SCOPE="$SCOPE%20eduperson_entitlement:urn:mace:egi.eu:group:$vo:role=vm_operator#aai.egi.eu"
+	SCOPE="$SCOPE%20eduperson_entitlement:urn:mace:egi.eu:group:$vo:role=member#aai.egi.eu"
+	OIDC_TOKEN=$(curl -X POST "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/token" \
+			  -d "grant_type=refresh_token&client_id=token-portal&scope=$SCOPE&refresh_token=$token" \
+			  | jq -r ".access_token")
+	echo "::add-mask::$OIDC_TOKEN"
+	echo $@
+	for cloud in "$@" ; do
+		echo $cloud
+		local site="$(yq -r ".clouds.$cloud.site" clouds.yaml)"
+		local vo="$(yq -r ".clouds.$cloud.vo" clouds.yaml)"
+		local os_token="$(fedcloud openstack token issue --oidc-access-token "$OIDC_TOKEN" \
+						--site "$site" --vo "$vo" -j | jq -r '.[0].Result.id')"
+		yq -y -i '.clouds.'"$cloud"'.auth.token="'"$os_token"'"'  clouds.yaml
+	done
 
-echo $OIDC_TOKEN
+	echo $OIDC_TOKEN
+}
