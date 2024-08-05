4e1884919436b60be7eb41c33783762f7746a9c6
Enol Fernandez <enol.fernandez@egi.eu>
2024-08-05 13:20:41 +0100

    Debug

--- builder/build-image.sh
+++ builder/build-image.sh
@@ -28,16 +28,16 @@ packer plugins install github.com/hashicorp/qemu
 packer plugins install github.com/hashicorp/ansible
 
 # do the build
-if tools/build.sh "$IMAGE" >/var/log/image-build.log 2>&1; then
-	builder/refresh.sh vo.access.egi.eu "$(cat /var/tmp/egi/.refresh_token)" images
-	OS_TOKEN="$(yq -r '.clouds.images.auth.token' /etc/openstack/clouds.yaml)"
-	VM_NAME="$(jq -r ".builders[].vm_name" < "$IMAGE")"
-	cd "$(dirname "$IMAGE")/output-qemu"
-	SHA="$(sha512sum -z $VM_NAME | cut -f1 -d" ")"
-	openstack --os-cloud images --os-token $OS_TOKEN \
-		object create egi_endorsed_vas \
-		"$VM_NAME" >>/var/log/image-build.log
-	echo "SUCCESSFUL BUILD - $VM_NAME - $SHA" >>/var/log/image-build.log
-fi
+#if tools/build.sh "$IMAGE" >/var/log/image-build.log 2>&1; then
+#	builder/refresh.sh vo.access.egi.eu "$(cat /var/tmp/egi/.refresh_token)" images
+#	OS_TOKEN="$(yq -r '.clouds.images.auth.token' /etc/openstack/clouds.yaml)"
+#	VM_NAME="$(jq -r ".builders[].vm_name" < "$IMAGE")"
+#	cd "$(dirname "$IMAGE")/output-qemu"
+#	SHA="$(sha512sum -z $VM_NAME | cut -f1 -d" ")"
+#	openstack --os-cloud images --os-token $OS_TOKEN \
+#		object create egi_endorsed_vas \
+#		"$VM_NAME" >>/var/log/image-build.log
+#	echo "SUCCESSFUL BUILD - $VM_NAME - $SHA" >>/var/log/image-build.log
+#fi
 
 echo "BUILD ENDED" >>/var/log/image-build.log
