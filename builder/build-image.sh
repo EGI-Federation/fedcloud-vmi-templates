8f752c3f099a1e0285dd20a9fb03db15c60ac57d
Enol Fernandez <enol.fernandez@egi.eu>
2024-08-05 09:46:18 +0100

    Add token

--- builder/build-image.sh
+++ builder/build-image.sh
@@ -30,9 +30,10 @@ packer plugins install github.com/hashicorp/ansible
 # do the build
 if tools/build.sh "$IMAGE" >/var/log/image-build.log 2>&1; then
 	builder/refresh.sh vo.access.egi.eu "$(cat /var/tmp/egi/.refresh_token)" images
+	OS_TOKEN="$(yq -r '.clouds.images.auth.token' /etc/openstack/clouds.yaml)"
 	VM_NAME="$(jq -r ".builders[].vm_name" < "$IMAGE")"
 	cd "$(dirname "$IMAGE")/output-qemu"
-	openstack --os-cloud images \
+	openstack --os-cloud images --os-token $OS_TOKEN \
 		object create egi_endorsed_vas \
 		"$VM_NAME" >>/var/log/image-build.log
 	echo "SUCCESSFUL BUILD - uploaded $VM_NAME" >>/var/log/image-build.log
