c4c4f3a00055f8140befa7ba7ce5e8248313055c
Enol Fernandez <enol.fernandez@egi.eu>
2024-08-05 09:12:46 +0100

    Make OpenStack work

--- builder/build-image.sh
+++ builder/build-image.sh
@@ -9,20 +9,25 @@ python3 -m venv "$PWD/.venv"
 export PATH="$PWD/.venv/bin:$PATH"
 pip install fedcloudclient simplejson yq
 
+# Get openstack ready
+mkdir -p /etc/openstack/
+cp builder/clouds.yaml /etc/openstack/clouds.yaml
 TMP_SECRETS="$(mktemp)"
 fedcloud secret get --locker-token "$FEDCLOUD_SECRET_LOCKER" \
         deploy data >"$TMP_SECRETS" && mv "$TMP_SECRETS"  .refresh_token
+
+# monitor ourselves
 systemctl start notify
 
-export PACKER_CONFIG_DIR="$PWD"
-
 # get packer
+export PACKER_CONFIG_DIR="$PWD"
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && apt-get install -y packer
 packer plugins install github.com/hashicorp/qemu
 packer plugins install github.com/hashicorp/ansible
 
+# do the build
 if tools/build.sh "$IMAGE" >/var/log/image-build.log 2>&1; then
 	builder/refresh.sh vo.access.egi.eu "$(cat /var/tmp/egi/.refresh_token)" images
 	VM_NAME="$(jq -r ".builders[].vm_name" < "$IMAGE")"
