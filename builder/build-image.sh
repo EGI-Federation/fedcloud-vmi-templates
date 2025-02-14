7c7919215e4d0adf2ad230b7c881d16fb13b5991
Enol Fernandez <enol.fernandez@egi.eu>
2025-02-14 10:23:38 +0000

    Only upload on push

--- builder/build-image.sh
+++ builder/build-image.sh
@@ -114,7 +114,7 @@ if tools/build.sh "$IMAGE"; then
 
     # All going well, upload the VMI in the registry
     # this should be done only if this is a push to main
-    if test "$UPLOAD" -eq 1; then
+    if test "$UPLOAD" == "true"; then
             builder/upload.sh "$IMAGE" secrets.json
     fi
     echo "### BUILD-RESULT: $(jq -cn --arg status "SUCCESS" \
